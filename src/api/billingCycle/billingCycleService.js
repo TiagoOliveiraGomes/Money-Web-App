255069acc859379a3dcef4d857f57d251df5124d
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-02-03 16:57:55 -0300

    add route count

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const billingCycle = require("./billingCycle")
 billingCycle.methods(['get', 'post', 'put', 'delete'])
 billingCycle.updateOptions({new: true, runValidators: true})
 
+billingCycle.route('count', (req, res, next)=> {
+    billingCycle.count((error, value)=>{
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
+
 module.exports = billingCycle
