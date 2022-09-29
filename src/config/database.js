a6dfb25194cc121a4c9b42ec3ad24accb7e3d420
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-09-29 16:54:38 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
+
+const password = process.env.password
 // mongoose.Promise = global.promise
 
-module.exports = mongoose.connect('mongodb://localhost/mymoney', {useMongoClient:true})
+// module.exports = mongoose.connect('mongodb://localhost/mymoney', {useMongoClient:true})
+module.exports = mongoose.connect(`mongodb+srv://MyMoneyFrontendMaster:${password}@MyMoneyApp.pv8brb9.mongodb.net/MyMoneyApp?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
--- src/config/server.js
+++ src/config/server.js
@@ -5,9 +5,11 @@ const express = require('express')
 const server = express()
 const allowCors = require("./cors")
 const queryParser = require("express-query-int")
+const cors = require('cors')
 
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
+server.use(cors())
 server.use(allowCors)
 server.use(queryParser())
 
