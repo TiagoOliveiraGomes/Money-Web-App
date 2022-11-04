b9a7e983d58b54564621494515c4bd0cc0e5eafa
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-04 20:56:10 -0300

    Update cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.header("Access-Allow-Control-Origin", "*")
     res.header("Access-Allow-Control-Methods", "GET, POST, PUT, OPTIONS, PATCH, DELETE")
-    res.header("Access-Allow-Control-Headers", "Origin, X-Request-With, Content-Type, Accept")
+    res.header("Access-Allow-Control-Headers", "Origin, X-Request-With, Content-Type, Accept, Authorization")
     next()
 }
