17e944e43bd264adcdbceb7bd88e052ce11549f6
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-11-04 21:03:36 -0300

    Update routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
-    const router = express.Router()
-    server.use('/api', router)
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
     const billingCycle = require('../api/billingCycle/billingCycleService')
     billingCycle.register(router, '/billingCycles')
+
+    const openApi = express.Router()
+    server.use('oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('login', AuthService.login)
+    openApi.post('signup', AuthService.signup)
+    openApi.post('validateToken', AuthService.validateToken)
 }
 
+
+// const router = express.Router()
+// server.use('/api', router)
